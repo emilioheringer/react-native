9e548c8128540f49194f2c0d0dcacdb89ca3e211
emilioheringer <emilioheringer@yahoo.com.br>
2022-03-26 17:31:45 +0000

    Chapter 1

---
+++ ContainersText/index.js
@@ -0,0 +1,8 @@
+/**
+ * @format
+ */
+
+ import React, { Component } from 'react';
+ import { AppRegistry } from 'react-native';
+ import MainApp from './src/MainApp';
+ AppRegistry.registerComponent('ContainersText', () => MainApp);
